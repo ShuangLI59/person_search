49838345d0ff3955682c54f5e0dddce19369fff2
Cysu <st.cysu@gmail.com>
2016-10-30 15:03:20 +0800

    Refactor to extract multiple feature blobs during test

--- lib/fast_rcnn/test_gallery.py
+++ lib/fast_rcnn/test_gallery.py
@@ -1,156 +1,55 @@
-from fast_rcnn.config import cfg, get_output_dir
-from fast_rcnn.bbox_transform import clip_boxes, bbox_transform_inv
-import argparse
-from utils.timer import Timer
 import numpy as np
 import cv2
-import caffe
+
+from fast_rcnn.config import cfg
+from fast_rcnn.bbox_transform import clip_boxes, bbox_transform_inv
 from fast_rcnn.nms_wrapper import nms
-import cPickle
-from utils.blob import im_list_to_blob
-import os
+from fast_rcnn.test_utils import get_image_blob, get_gt_boxes_blob
+from utils.timer import Timer
 
-def _get_image_blob(im):
-    """Converts an image into a network input.
 
-    Arguments:
-        im (ndarray): a color image in BGR order
-
-    Returns:
-        blob (ndarray): a data blob holding an image pyramid
-        im_scale_factors (list): list of image scales (relative to im) used
-            in the image pyramid
-    """
-    im_orig = im.astype(np.float32, copy=True)
-    im_orig -= cfg.PIXEL_MEANS
-
-    im_shape = im_orig.shape
-    im_size_min = np.min(im_shape[0:2])
-    im_size_max = np.max(im_shape[0:2])
-
-    processed_ims = []
-    im_scale_factors = []
-
-    for target_size in cfg.TEST.SCALES:
-        im_scale = float(target_size) / float(im_size_min)
-        # Prevent the biggest axis from being more than MAX_SIZE
-        if np.round(im_scale * im_size_max) > cfg.TEST.MAX_SIZE:
-            im_scale = float(cfg.TEST.MAX_SIZE) / float(im_size_max)
-        im = cv2.resize(im_orig, None, None, fx=im_scale, fy=im_scale,
-                        interpolation=cv2.INTER_LINEAR)
-        im_scale_factors.append(im_scale)
-        processed_ims.append(im)
-
-    # Create a blob to hold the input images
-    blob = im_list_to_blob(processed_ims)
-
-    return blob, np.array(im_scale_factors)
-
-def _get_rois_blob(im_rois, im_scale_factors):
-    """Converts RoIs into network inputs.
-
-    Arguments:
-        im_rois (ndarray): R x 4 matrix of RoIs in original image coordinates
-        im_scale_factors (list): scale factors as returned by _get_image_blob
-
-    Returns:
-        blob (ndarray): R x 5 matrix of RoIs in the image pyramid
-    """
-    rois, levels = _project_im_rois(im_rois, im_scale_factors)
-    rois_blob = np.hstack((levels, rois))
-    return rois_blob.astype(np.float32, copy=False)
-
-def _project_im_rois(im_rois, scales):
-    """Project image RoIs into the image pyramid built by _get_image_blob.
-
-    Arguments:
-        im_rois (ndarray): R x 4 matrix of RoIs in original image coordinates
-        scales (list): scale factors as returned by _get_image_blob
-
-    Returns:
-        rois (ndarray): R x 4 matrix of projected RoI coordinates
-        levels (list): image pyramid levels used by each projected RoI
-    """
-    im_rois = im_rois.astype(np.float, copy=False)
-
-    if len(scales) > 1:
-        widths = im_rois[:, 2] - im_rois[:, 0] + 1
-        heights = im_rois[:, 3] - im_rois[:, 1] + 1
-
-        areas = widths * heights
-        scaled_areas = areas[:, np.newaxis] * (scales[np.newaxis, :] ** 2)
-        diff_areas = np.abs(scaled_areas - 224 * 224)
-        levels = diff_areas.argmin(axis=1)[:, np.newaxis]
-    else:
-        levels = np.zeros((im_rois.shape[0], 1), dtype=np.int)
-
-    rois = im_rois * scales[levels]
-
-    return rois, levels
-
-def _get_blobs(im, rois):
-    """Convert an image and RoIs within that image into network inputs."""
-    blobs = {'data' : None, 'rois' : None}
-    blobs['data'], im_scale_factors = _get_image_blob(im)
-    if not cfg.TEST.HAS_RPN:
-        blobs['rois'] = _get_rois_blob(rois, im_scale_factors)
-    return blobs, im_scale_factors
-
-def im_detect(net, im, feat_blob, boxes=None):
+def _im_detect(net, im, roidb, blob_names=None):
     """Detect object classes in an image given object proposals.
 
     Arguments:
         net (caffe.Net): Fast R-CNN network to use
         im (ndarray): color image to test (in BGR order)
-        feat_blob (str): name of the feature blob to be extracted
-        boxes (ndarray): R x 4 array of object proposals or None (for RPN)
+        roidb (an roidb item): to provide gt_boxes if necessary
+        blob_names (list of str): list of feature blob names to be extracted
 
     Returns:
+        boxes (ndarray): R x (4*K) array of predicted bounding boxes
         scores (ndarray): R x K array of object class scores (K includes
             background as object category 0)
-        boxes (ndarray): R x (4*K) array of predicted bounding boxes
-        features (ndarray): R x D array of features
+        features (dict of ndarray): {blob name: R x D array of features}
     """
-    blobs, im_scales = _get_blobs(im, boxes)
+    im_blob, im_scales = get_image_blob(im)
+    assert len(im_scales) == 1, "Only single-image batch implemented"
 
-    # When mapping from image ROIs to feature map ROIs, there's some aliasing
-    # (some distinct image ROIs get mapped to the same feature ROI).
-    # Here, we identify duplicate feature ROIs, so we only compute features
-    # on the unique subset.
-    if cfg.DEDUP_BOXES > 0 and not cfg.TEST.HAS_RPN:
-        v = np.array([1, 1e3, 1e6, 1e9, 1e12])
-        hashes = np.round(blobs['rois'] * cfg.DEDUP_BOXES).dot(v)
-        _, index, inv_index = np.unique(hashes, return_index=True,
-                                        return_inverse=True)
-        blobs['rois'] = blobs['rois'][index, :]
-        boxes = boxes[index, :]
-
-    if cfg.TEST.HAS_RPN:
-        im_blob = blobs['data']
-        blobs['im_info'] = np.array(
+    blobs = {
+        'data': im_blob,
+        'im_info': np.array(
             [[im_blob.shape[2], im_blob.shape[3], im_scales[0]]],
-            dtype=np.float32)
+            dtype=np.float32),
+    }
+
+    if 'gt_boxes' in net.blobs:
+        # Supply gt_boxes as input. Used to get pid_labels for proposals.
+        blobs['gt_boxes'] = get_gt_boxes_blob(
+            roidb['boxes'], roidb['gt_pids'], im_scales)
 
     # reshape network inputs
-    net.blobs['data'].reshape(*(blobs['data'].shape))
-    if cfg.TEST.HAS_RPN:
-        net.blobs['im_info'].reshape(*(blobs['im_info'].shape))
-    else:
-        net.blobs['rois'].reshape(*(blobs['rois'].shape))
+    for k, v in blobs.iteritems():
+        net.blobs[k].reshape(*(v.shape))
 
     # do forward
-    forward_kwargs = {'data': blobs['data'].astype(np.float32, copy=False)}
-    if cfg.TEST.HAS_RPN:
-        forward_kwargs['im_info'] = blobs['im_info'].astype(np.float32, copy=False)
-    else:
-        forward_kwargs['rois'] = blobs['rois'].astype(np.float32, copy=False)
+    forward_kwargs = {k: v.astype(np.float32, copy=False)
+                      for k, v in blobs.iteritems()}
     blobs_out = net.forward(**forward_kwargs)
 
-    if cfg.TEST.HAS_RPN:
-        assert len(im_scales) == 1, "Only single-image batch implemented"
-        rois = net.blobs['rois'].data.copy()
-        # unscale back to raw image space
-        boxes = rois[:, 1:5] / im_scales[0]
+    # unscale rois back to raw image space
+    rois = net.blobs['rois'].data.copy()
+    boxes = rois[:, 1:5] / im_scales[0]
 
     if cfg.TEST.SVM:
         # use the raw scores before softmax under the assumption they
@@ -173,23 +72,19 @@ def im_detect(net, im, feat_blob, boxes=None):
             stds = np.tile(cfg.TRAIN.BBOX_NORMALIZE_STDS, num_classes)
             means = np.tile(cfg.TRAIN.BBOX_NORMALIZE_MEANS, num_classes)
             box_deltas = box_deltas * stds + means
-        pred_boxes = bbox_transform_inv(boxes, box_deltas)
-        pred_boxes = clip_boxes(pred_boxes, im.shape)
+        boxes = bbox_transform_inv(boxes, box_deltas)
+        boxes = clip_boxes(boxes, im.shape)
     else:
         # Simply repeat the boxes, once for each class
-        pred_boxes = np.tile(boxes, (1, scores.shape[1]))
+        boxes = np.tile(boxes, (1, scores.shape[1]))
 
-    features = net.blobs[feat_blob].data.copy()
+    features = {blob: net.blobs[blob].data.copy() for blob in blob_names} \
+               if blob_names is not None else {}
 
-    if cfg.DEDUP_BOXES > 0 and not cfg.TEST.HAS_RPN:
-        # Map scores and predictions back to the original set of boxes
-        scores = scores[inv_index, :]
-        pred_boxes = pred_boxes[inv_index, :]
-        features = features[inv_index, :]
+    return boxes, scores, features
 
-    return scores, pred_boxes, features
 
-def vis_detections(im, class_name, dets, thresh=0.3):
+def _vis_detections(im, class_name, dets, thresh=0.3):
     """Visual debugging of detections."""
     import matplotlib.pyplot as plt
     im = im[:, :, (2, 1, 0)]
@@ -208,70 +103,55 @@ def vis_detections(im, class_name, dets, thresh=0.3):
             plt.title('{}  {:.3f}'.format(class_name, score))
             plt.show()
 
-def test_net_on_gallery_set(net, imdb, start, end, feat_blob,
-                            max_per_image=100, thresh=0.05, vis=False):
-    assert imdb.num_classes == 2, "Only support pedestrian detection (two classes)"
+
+def detect_and_exfeat(net, imdb,
+                      start=None, end=None,
+                      blob_names=None,
+                      thresh=0.05, vis=False):
+    assert imdb.num_classes == 2, "Only support two-class detection"
+    assert cfg.TEST.HAS_RPN, "Only support RPN as proposal"
+
+    start = start or 0
+    end = end or imdb.num_images
     num_images = end - start
+
     # all detections are collected into:
     #    all_boxes[image] = N x 5 array of detections in
     #    (x1, y1, x2, y2, score)
-    #    all_features[image] = N x D array of features
+    #    all_features[blob][image] = N x D array of features
     all_boxes = [0 for _ in xrange(num_images)]
-    all_features = [0 for _ in xrange(num_images)]
+    all_features = {} if blob_names is None else \
+                   {blob: [0 for _ in xrange(num_images)]
+                    for blob in blob_names}
 
     # timers
     _t = {'im_detect' : Timer(), 'misc' : Timer()}
 
-    if not cfg.TEST.HAS_RPN:
-        roidb = imdb.roidb
-
     for i in xrange(num_images):
-        im_index = start + i
-        # filter out any ground truth boxes
-        if cfg.TEST.HAS_RPN:
-            box_proposals = None
-        else:
-            # The roidb may contain ground-truth rois (for example, if the roidb
-            # comes from the training or val split). We only want to evaluate
-            # detection on the *non*-ground-truth rois. We select those the rois
-            # that have the gt_classes field set to 0, which means there's no
-            # ground truth.
-            box_proposals = roidb[im_index]['boxes'][roidb[im_index]['gt_classes'] == 0]
+        im = cv2.imread(imdb.image_path_at(start + i))
+        roidb = imdb.roidb[start + i]
 
-        im = cv2.imread(imdb.image_path_at(im_index))
         _t['im_detect'].tic()
-        scores, boxes, features = im_detect(net, im, feat_blob, box_proposals)
+        boxes, scores, feat_dic = _im_detect(net, im, roidb, blob_names)
         _t['im_detect'].toc()
 
         _t['misc'].tic()
-
-        j = 1  # only consider j = 1 (pedestrian class)
+        j = 1  # only consider j = 1 (foreground class)
         inds = np.where(scores[:, j] > thresh)[0]
         cls_scores = scores[inds, j]
         cls_boxes = boxes[inds, j*4:(j+1)*4]
         cls_dets = np.hstack((cls_boxes, cls_scores[:, np.newaxis])) \
             .astype(np.float32, copy=False)
         keep = nms(cls_dets, cfg.TEST.NMS)
-        cls_dets = cls_dets[keep, :]
-        if vis:
-            vis_detections(im, imdb.classes[j], cls_dets)
-        all_boxes[i] = cls_dets
-        all_features[i] = features[keep, :]
-
-        # Limit to max_per_image detections *over all classes*
-        if max_per_image > 0:
-            image_scores = np.hstack([all_boxes[i][:, -1]
-                                      for j in xrange(1, imdb.num_classes)])
-            if len(image_scores) > max_per_image:
-                image_thresh = np.sort(image_scores)[-max_per_image]
-                for j in xrange(1, imdb.num_classes):
-                    keep = np.where(all_boxes[i][:, -1] >= image_thresh)[0]
-                    all_boxes[i] = all_boxes[i][keep, :]
-                    all_features[i] = all_features[i][keep, :]
+        all_boxes[i] = cls_dets[keep, :]
+        for blob, feat in feat_dic.iteritems():
+            all_features[blob][i] = feat[keep, :]
         _t['misc'].toc()
 
-        print 'im_detect: {:d}/{:d} {:.3f}s {:.3f}s' \
-              .format(i + 1, num_images, _t['im_detect'].average_time,
-                      _t['misc'].average_time)
+        print 'im_detect: {:d}/{:d} {:.3f}s {:.3f}s'.format(i + 1, num_images,
+            _t['im_detect'].average_time, _t['misc'].average_time)
+
+        if vis:
+            _vis_detections(im, imdb.classes[j], all_boxes[i])
 
     return all_boxes, all_features
