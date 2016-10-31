720a665daf87646273ae622d8ff3838547cc01e0
Cysu <st.cysu@gmail.com>
2016-10-31 16:39:32 +0800

    Remove other datasets from factory

--- lib/datasets/factory.py
+++ lib/datasets/factory.py
@@ -9,22 +9,8 @@
 
 __sets = {}
 
-from datasets.pascal_voc import pascal_voc
-from datasets.coco import coco
 from datasets.psdb import psdb
 import numpy as np
-
-# Set up voc_<year>_<split> using selective search "fast" mode
-for year in ['2007', '2012']:
-    for split in ['train', 'val', 'trainval', 'test']:
-        name = 'voc_{}_{}'.format(year, split)
-        __sets[name] = (lambda split=split, year=year: pascal_voc(split, year))
-
-# Set up coco_2014_<split>
-for year in ['2014']:
-    for split in ['train', 'val', 'minival', 'valminusminival']:
-        name = 'coco_{}_{}'.format(year, split)
-        __sets[name] = (lambda split=split, year=year: coco(split, year))
 
 # Set up psdb_<split>
 for split in ['train', 'test']:
