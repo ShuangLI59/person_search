c50d723ff97ed75256b574269b5e441693b88cdb
Cysu <st.cysu@gmail.com>
2016-11-07 00:45:05 +0800

    Use mpirun

--- experiments/scripts/train_resnet50.sh
+++ experiments/scripts/train_resnet50.sh
@@ -37,7 +37,7 @@ LOG="experiments/logs/${DATASET}_train_${NET}_${EXTRA_ARGS_SLUG}.txt.`date +'%Y-
 exec &> >(tee -a "$LOG")
 echo Logging output to "$LOG"
 
-time python2 tools/train_net.py --gpu ${GPU_ID} \
+time mpirun -n 8 python2 tools/train_net.py --gpu ${GPU_ID} \
   --solver models/${PT_DIR}/${NET}/solver.prototxt \
   --weights data/imagenet_models/${NET}.caffemodel \
   --imdb ${TRAIN_IMDB} \
