ff56985453087cb2a16a3786d2649b0be2e5e665
Cysu <st.cysu@gmail.com>
2016-11-07 00:37:36 +0800

    Ready for experiments with half BN open

---
+++ experiments/scripts/train_resnet50.sh
@@ -0,0 +1,51 @@
+#!/usr/bin/env bash
+# Usage:
+# ./experiments/scripts/train.sh GPU [options args to {train,test}_net.py]
+#
+# Example:
+# ./experiments/scripts/train.sh 0 \
+#   --set EXP_DIR foobar RNG_SEED 42 TRAIN.SCALES "[400, 500, 600, 700]"
+
+set -x
+set -e
+
+export PYTHONUNBUFFERED="True"
+
+GPU_ID=$1
+NET=resnet50
+DATASET=psdb
+
+array=( $@ )
+len=${#array[@]}
+EXTRA_ARGS=${array[@]:1:$len}
+EXTRA_ARGS_SLUG=${EXTRA_ARGS// /_}
+
+case $DATASET in
+  psdb)
+    TRAIN_IMDB="psdb_train"
+    TEST_IMDB="psdb_test"
+    PT_DIR="psdb"
+    ITERS=50000
+    ;;
+  *)
+    echo "No dataset given"
+    exit
+    ;;
+esac
+
+LOG="experiments/logs/${DATASET}_train_${NET}_${EXTRA_ARGS_SLUG}.txt.`date +'%Y-%m-%d_%H-%M-%S'`"
+exec &> >(tee -a "$LOG")
+echo Logging output to "$LOG"
+
+time python2 tools/train_net.py --gpu ${GPU_ID} \
+  --solver models/${PT_DIR}/${NET}/solver.prototxt \
+  --weights data/imagenet_models/${NET}.caffemodel \
+  --imdb ${TRAIN_IMDB} \
+  --iters ${ITERS} \
+  --cfg experiments/cfgs/train_resnet50.yml \
+  --rand \
+  ${EXTRA_ARGS}
+
+set +x
+NET_FINAL=`grep -B 1 "done solving" ${LOG} | grep "Wrote snapshot" | awk '{print $4}'`
+set -x
